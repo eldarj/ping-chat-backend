8dcf0d6f92117ecfc00a595e068611cc9077eb81
Eldar Jahijagic <eldar.jahijagic@infobip.com>
2020-12-16 22:32:39 +0100

    Adjust chat controller

--- src/main/java/com/pingchat/authenticationservice/api/rest/ChatController.java
+++ src/main/java/com/pingchat/authenticationservice/api/rest/ChatController.java
@@ -3,7 +3,6 @@ package com.pingchat.authenticationservice.api.rest;
 
 import com.pingchat.authenticationservice.model.dto.MessageDto;
 import com.pingchat.authenticationservice.model.event.PresenceEvent;
-import com.pingchat.authenticationservice.service.data.ContactDataService;
 import com.pingchat.authenticationservice.service.data.MessageDataService;
 import com.pingchat.authenticationservice.service.memory.PresenceInMemoryService;
 import com.pingchat.authenticationservice.util.pagination.PagedSearchResult;
@@ -16,14 +15,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
-    private final ContactDataService contactDataService;
     private final MessageDataService messageDataService;
     private final PresenceInMemoryService presenceInMemoryService;
 
-    public ChatController(ContactDataService contactDataService,
-                          MessageDataService messageDataService,
+    public ChatController(MessageDataService messageDataService,
                           PresenceInMemoryService presenceInMemoryService) {
-        this.contactDataService = contactDataService;
         this.messageDataService = messageDataService;
         this.presenceInMemoryService = presenceInMemoryService;
     }
@@ -32,10 +28,6 @@ public class ChatController {
     public PagedSearchResult<MessageDto> findRecentChats(@PathVariable Long userId,
                                                          @RequestParam Integer pageSize,
                                                          @RequestParam Integer pageNumber) {
-//        return messageDataService.findRecentSentOrReceived(userId,
-//                PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Direction.DESC, "sentTimestamp")));
-
-
         return messageDataService.findRecentSentOrReceived(userId, pageSize, pageNumber);
     }
 
@@ -43,15 +35,4 @@ public class ChatController {
     public List<PresenceEvent> findPresenceStatuses(@RequestParam List<String> phoneNumbers) {
         return presenceInMemoryService.getPresences(phoneNumbers);
     }
-
-//    @GetMapping("/{receiverPhoneNumber}/{pageNumber}")
-//    public Page<MessageDto> findMessagesByContactId(@PathVariable String receiverPhoneNumber,
-//                                                    @PathVariable int pageNumber) {
-//        String senderPhoneNumber = SecurityContextUserProvider.currentUserPrincipal();
-//        return messageDataService.findBySenderOrReceiver(
-//                senderPhoneNumber,
-//                receiverPhoneNumber,
-//                PageRequest.of(pageNumber, 20, Sort.by(Sort.Direction.DESC, "sentTimestamp"))
-//        );
-//    }
 }
